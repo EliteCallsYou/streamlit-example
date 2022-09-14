51a6930f8fcf5920b885498e80f6a5e74694ff1f
Shuhrat <79278760+EliteCallsYou@users.noreply.github.com>
2022-09-14 16:19:38 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,38 @@
-from collections import namedtuple
-import altair as alt
-import math
+# Predicting salary #
+
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+data = pd.DataFrame(data={
+    'experience':[1,3,5,10],
+    'gender':[1,0,0,1],
+    'salary':[1000,3000,5200,9800]
+})
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+X = data.drop('salary',axis=1)
+y = data['salary']
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+from sklearn.linear_model import LinearRegression
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+model = LinearRegression()
+model.fit(X,y)
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+xp = st.slider( 'years of experience:',0,12 )
+sex = st.selectbox( 'your gender:',['Male','Female'] )
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+if sex == 'Male' :
+    sex = 1
+else :
+    sex = 0
 
-    points_per_turn = total_points / num_turns
+    
+predict = pd.DataFrame( data={
+    'experience':[xp],
+    'gender':[sex]
+})
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+if ( xp ) :
+    for i in range(10) :st.text('')
+    st.text( 'Your estimated salary is: {}'.format( int(model.predict(predict)[0]) ) )
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
