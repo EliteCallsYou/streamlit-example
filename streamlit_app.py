b00a02440f855b7f6a58fc8eea36923133a81756
Shuhrat <79278760+EliteCallsYou@users.noreply.github.com>
2022-10-08 15:08:18 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,99 +1,108 @@
-##########################################################################################################
-
-import pandas as pd
+##############################
 import streamlit as st
-from PIL import Image
-import cv2 as cv
+import pandas as pd
+import pyreadstat
+import pickle
+import catboost
+from catboost import CatBoostClassifier
+from sklearn import preprocessing
+##############################
 
-#####################################################
+if 'count' not in st.session_state :
+
+	st.session_state['count'] = 0
+
+	data = pd.read_excel( 'new_5th_variant_v2.xlsx' )
+
+	loaded_model = pickle.load(open('final_cluster.pickle', 'rb'))
+
+	variables = ['nom_q_9103f','nom_q_9103o','nom_q_9103b','nom_q_9103k',
+	             'nom_q_9103s','nom_q_9103h','nom_q_9103c','nom_q_9103l','nom_q_9103j',
+	             'nom_q_9103q','nom_q_9103m','nom_q_9103p','nom_q_9103g','nom_q_9103e','nom_q_9103r','nom_q_9103n','nom_q_9103d','nom_q_9103i',
+	             'ord_T_q_9101c_1','ord_T_q_9101e_7','ord_T_q_9101a_7','ord_T_q_9101d_9','ord_T_q_9101a_8','ord_T_q_9101f_2','ord_T_q_9101d_2',
+	             'ord_T_q_9101c_4','ord_T_q_9101d_3','ord_T_q_9101c_9','ord_T_q_9101d_4','ord_T_q_9101c_7','ord_T_q_9101b_3','ord_T_q_9101b_2',
+	             'ord_T_q_9101e_8','ord_T_q_9101a_5','ord_T_q_9101a_3','ord_T_q_9101b_6','ord_T_q_9101e_4','ord_T_q_9101b_7','ord_T_q_9101a_4',
+	             'ord_T_q_9101c_8','ord_T_q_9101c_2','ord_T_q_9101c_6','ord_T_q_9101d_7','ord_T_q_9101e_2','ord_T_q_9101e_1','ord_T_q_9101d_6',
+	             'ord_T_q_9101d_8','ord_T_q_9101b_1','ord_T_q_9101b_4']
+
+	variables.append('nom_q_3222code')
+
+	main = ['1110','1102','1111','9301','9305','9303','9306','9308','serv_SbjNum']
+	for i in data.columns :
+	    for j in main :
+	        if type(i) == int :
+	            continue
+	        if j in i :
+	            variables.append(i)
+	            break
+
+	for i in data.columns :
+	    if i not in variables :
+	        data.drop( i, axis=1,inplace=True )
 
 
-original_title = "<p style='font-family:Courier; color:Blue; font-size: 16px;'>Which image from the pj20_jets mission do u want</p>"
-st.markdown(original_title, unsafe_allow_html=True)
-image_name = st.selectbox( '',
- ('', 'JNCE_2019149_20C00028_V01-raw','JNCE_2019149_20C00030_V01-raw',
- 'JNCE_2019149_20C00043_V01-raw','JNCE_2019149_20C00045_V01-raw',
- 'JNCE_2019149_20C00046_V01-raw','JNCE_2019149_20C00048_V01-raw' ), label_visibility='collapsed')
+	######################################################################################
 
-if image_name != '' :
-	dic_json = {
-		'JNCE_2019149_20C00028_V01-raw':'20-28.json',
-		'JNCE_2019149_20C00030_V01-raw':'20-30.json',
-		'JNCE_2019149_20C00043_V01-raw':'20-43.json',
-		'JNCE_2019149_20C00045_V01-raw':'20-45.json',
-		'JNCE_2019149_20C00046_V01-raw':'20-46.json',
-		'JNCE_2019149_20C00048_V01-raw':'20-48.json'
-	}
-	img_json = dic_json[ image_name ]
-	image_name += '.png'
+	df, meta = pyreadstat.read_sav('SAV_format.sav')
 
-	img_json = 'json_files/' + img_json
-	image_name = 'images/'+image_name
+	dic = meta.column_names_to_labels # Dictionary of variables and labels
 
-	#####################################################
+	label = {}
+
+	for i in range ( len( meta.column_names ) ) :
+	    label[ meta.column_names[i] ] = meta.column_labels[i]
+
+	data.drop( ['serv_SbjNum','nom_q_3222code'],axis=1,inplace=True )
 
 
-	if 'last' not in st.session_state:
-		st.session_state['last'] = '#'
-
-	if st.session_state['last'] != image_name :
-		from Example1 import func_hello
-		func_hello( image_name, img_json )
-		st.session_state['last'] = image_name
+	#######################################
+	st.session_state['data'] = data
+	st.session_state['loaded_model'] = loaded_model
+	st.session_state['label'] = label
 
 
-	def change_brightness(img, value):
-		hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-		h, s, v = cv.split(hsv)
-		v = cv.add(v,value)
-		v[v > 255] = 255
-		v[v < 0] = 0
-		final_hsv = cv.merge((h, s, v))
-		img = cv.cvtColor(final_hsv, cv.COLOR_HSV2BGR)
-		return img
 
-	def blur( img, value ) :
-		if value <= 0 :
-			value = value*-1
-			value = value + (value-1)%2
-			img = cv.GaussianBlur(img,(value,value),0)
-		else :
-			value_ = value*0.01
-			img = cv.detailEnhance(img, sigma_s=10, sigma_r=value_)
-		return img
+data = st.session_state['data']
+loaded_model = st.session_state['loaded_model']
+label = st.session_state['label']
+######################################################################################
 
-	####################################################
 
-	link = '/home/elitecallsyou/Desktop/Nasa/Example1.png'
-	img = cv.imread(link)
+user_data = pd.DataFrame()
 
-	#####################################################
+for i in data.columns :
+	ans = st.selectbox( label[i], options = list(data[i].value_counts().index) )
+	user_data[ i ] = [ ans ]
 
-	#########################################################
-	st.write('')
-	original_title = "<p style='font-family:Courier; color:orange; font-size: 16px;'>Contrast:</p>"
-	st.markdown(original_title, unsafe_allow_html=True)
+data = pd.concat( [user_data,data] )
 
-	brightness = st.slider(label='', min_value=-50, max_value=50, value=0,label_visibility='collapsed')
+data.index = range( len(data) )
 
-	#########################################################
+#############################################
+#			Predicting 3222  				#
+#############################################
 
-	st.write('')
-	original_title = "<p style='font-family:Courier; color:Brown; font-size: 16px;'>Blurness:</p>"
-	st.markdown(original_title, unsafe_allow_html=True)
+for i in data.columns :
+    if ('nom' not in i) :
+        continue
+    ###################
+    dummy = pd.get_dummies( data[i], prefix = i )
+    data = pd.concat( [data,dummy], axis=1 )
+    data.drop( i, axis = 1, inplace = True )
 
-	blurness = st.slider(label='', min_value=-20,max_value=20,value=0,label_visibility='collapsed' )
 
-	#########################################################
+for i in data.columns :
+    if ( 'ord' not in i ) :
+        continue
+    a=list( data[i].unique() )
+    b=list( range(len(a)) )
+    d=dict(zip(a,b))
+    data[i] = data[i].map(d)
 
-	img = change_brightness( img,value=brightness )
-	img = blur( img,blurness )
+## Predicting ##
 
-	######################################
+output = loaded_model.predict( data[0:1] )[0][0]
 
-	# # Saving file and opening through a PIL #
-	cv.imwrite( '/home/elitecallsyou/Desktop/Nasa/result.png', img )
-	image = Image.open('/home/elitecallsyou/Desktop/Nasa/result.png')
-
-	# Streamlit output image #
-	st.image(image, caption=None, width=None,use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+st.write()
+st.write()
+st.write( 'Your cluster is : ' + str(output) )
